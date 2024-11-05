577144a15b0827c63ae51b2d547ae0ba62c6bf41
Krish Sethi <95207265+Krish-198@users.noreply.github.com>
2024-11-05 19:13:49 +0530

    Update L9.py

--- L9.py
+++ L9.py
@@ -1,40 +1,62 @@
-import cv2,os,numpy
-har="haarcascade_frontalface_default.xml"
-data="Data_sets"
-print("Recognizing face please be in sufficient light")
-(images,labels,names,id) = ([],[],{},0)
-for(subdir,dir,file) in os.walk(data):
-    for subdir in dir:
-        names[id]=subdir
-        path=os.path.join(data,subdir)
-        for filename in os.listdir(path):
-            pp=path+'/'+filename
-            label=id
-            images.append(cv2.imread(pp,0))
+# It helps in identifying the faces
+import cv2, sys, numpy, os
+size = 4
+haar_file = 'haarcascade_frontalface_default.xml'
+datasets = 'Data_sets'
+ 
+print(cv2.__version__)
+# Part 1: Create fisherRecognizer
+print('Recognizing Face Please Be in sufficient Lights...')
+ 
+# Create a list of images and a list of corresponding names
+(images, labels, names, id) = ([], [], {}, 0)
+for (subdirs, dirs, files) in os.walk(datasets):
+    for subdir in dirs:
+        names[id] = subdir
+        subjectpath = os.path.join(datasets, subdir)
+        for filename in os.listdir(subjectpath):
+            path = subjectpath + '/' + filename
+            label = id
+            images.append(cv2.imread(path, 0))
             labels.append(int(label))
-        id+=1
-(width,height) = (130,100)
-(images,labels) = [numpy.array(i) for i in [images,labels]]
-r=cv2.face.LBPHFaceRecognizer_create()
-r.train(images,labels)
-face=cv2.CascadeClassifier(har)
-print("Please turn on your camera ")
-webcam=cv2.VideoCapture(0)
+        id += 1
+(width, height) = (130, 100)
+ 
+# Create a Numpy array from the two lists above
+(images, labels) = [numpy.array(lis) for lis in [images, labels]]
+ 
+# OpenCV trains a model from the images
+# NOTE FOR OpenCV2: remove '.face'
 
-
-while  True:
-    (c,d)=webcam.read()
-    gray=cv2.cvtColor(d,cv2.COLOR_BGR2GRAY)
-    faces=face.detectMultiScale(gray,1.3,4)
-    for (x,y,w,h) in faces:
-        cv2.rectangle(d,(x,y),(x+w,y+h),(0,0,255),3)
-        g=gray[y:y+h,x:x+w]
-        faceresize=cv2.resize(g,(width,height))
-        detect=r.predict(faceresize)
-        cv2.rectangle(d,(x,y),(x+w,y+h),(0,0,255),3) 
-        if detect[1]:
-            cv2.putText(d,(names[detect[0]],detect[1]))
-            
-
-
-    cv2.imshow("Krish",d)
+model = cv2.face.LBPHFaceRecognizer_create()
+model.train(images, labels)
+ 
+# Part 2: Use fisherRecognizer on camera stream
+face_cascade = cv2.CascadeClassifier(haar_file)
+webcam = cv2.VideoCapture(0)
+while True:
+    (_, im) = webcam.read()
+    gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(im, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        face = gray[y:y + h, x:x + w]
+        face_resize = cv2.resize(face, (width, height))
+        # Try to recognize the face
+        prediction = model.predict(face_resize)
+        cv2.rectangle(im, (x, y), (x + w, y + h), (0, 255, 0), 3)
+ 
+        if prediction[1]<500:
+ 
+           cv2.putText(im, '% s - %.0f' %
+(names[prediction[0]], prediction[1]), (x-10, y-10),
+cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0))
+        else:
+          cv2.putText(im, 'not recognized',
+(x-10, y-10), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0))
+ 
+    cv2.imshow('OpenCV', im)
+     
+    key = cv2.waitKey(10)
+    if key == 27:
+        break
